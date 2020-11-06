c2942384a72da22b5ecba943738d9b4b2fca5583
Aleix Diaz Baggerman <aleixdiazbaggerman@gmail.com>
2020-11-06 10:34:03 +0100

    [CORE] Project structure created

---
+++ API/src/Hypervideo.js
@@ -0,0 +1,15 @@
+class Hypervideo {
+
+    constructor(videoURL, containerID) {
+        this.videoURL = videoURL;
+        this.containerID = containerID;
+    }
+
+    setupHypervideo(tagsJSON) {
+        //Pensar si això ho necessitare guardar o no
+        this.tagsJSON = tagsJSON;
+
+    }
+    
+
+}
---
+++ Examples/SimpleTest/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Dom loaded")
+
+    const hypervideo = new Hypervideo();
+
+    hypervideo._test2();
+
+
+})
