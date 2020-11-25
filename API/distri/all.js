e368c582f2c6a36b42f5444f40071a8df8d8dad3
Aleix Diaz Baggerman <aleixdiazbaggerman@gmail.com>
2020-11-25 09:30:01 +0100

    [CORE] Setup clickable progress bar

--- API/distri/all.js
+++ API/distri/all.js
@@ -541,17 +541,24 @@ class XProgressBar extends HTMLElement {
         super();
         this.htmlManager = new HTMLManager();
         this.maxLength = 100;
+        this.currentLength = 0;
         let shadow = this.attachShadow({mode: 'open'});
-        
+
+        this.addEventListener('click', this.onClickBar);
         const bar = this.htmlManager.createElement("div", ["progress-bar"]);
         shadow.appendChild(this.getStyle());
         shadow.appendChild(bar);
+    }
 
-        this.setMaxLength(200);
-        this.setCurrentLength(100);
+    onClickBar(event) {
+        const rect = event.target.getBoundingClientRect();
+        const posX = event.clientX - rect.left;
+        const progress = posX / rect.width;
+        this.setCurrentLength(progress * this.maxLength);
     }
 
     setCurrentLength(length) {
+        this.currentLength = length;
         const progressBar = this.shadowRoot.querySelector(".progress-bar");
         const progress = (length / this.maxLength) * 100;
         progressBar.style.width = progress + "%";
@@ -559,6 +566,14 @@ class XProgressBar extends HTMLElement {
 
     setMaxLength(length) {
         this.maxLength = length;
+    }
+
+    increment(value) {
+        let inc = 1;
+        if (value !== undefined && value !== null && !isNaN(value)) {
+            inc = value;
+        }
+        this.setCurrentLength(this.currentLength + inc);
     }
 
     getStyle() {
--- API/src/XProgressBar.js
+++ API/src/XProgressBar.js
@@ -6,10 +6,22 @@ class XProgressBar extends HTMLElement {
         this.maxLength = 100;
         this.currentLength = 0;
         let shadow = this.attachShadow({mode: 'open'});
-        
+
+        this.addEventListener('click', this.onClickBar);
         const bar = this.htmlManager.createElement("div", ["progress-bar"]);
         shadow.appendChild(this.getStyle());
         shadow.appendChild(bar);
+    }
+
+    addMarkerAt(length) {
+        
+    }
+
+    onClickBar(event) {
+        const rect = event.target.getBoundingClientRect();
+        const posX = event.clientX - rect.left;
+        const progress = posX / rect.width;
+        this.setCurrentLength(progress * this.maxLength);
     }
 
     setCurrentLength(length) {
