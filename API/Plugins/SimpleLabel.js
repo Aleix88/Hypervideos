9e2c21b4cd84374e9e5527ff23abe5284cf86c88
Aleix88 <aleixdiazbaggerman@gmail.com>
2020-12-26 14:27:35 +0100

    [BUG] SimpleLabel scroll position added

--- API/Plugins/SimpleLabel.js
+++ API/Plugins/SimpleLabel.js
@@ -33,8 +33,10 @@ class SimpleLabel {
         const containerLeft = this.container.getBoundingClientRect().left;
         const containerHeight = this.container.getBoundingClientRect().height;
         const containerWidth = this.container.getBoundingClientRect().width;
-        const targetTop = target.getBoundingClientRect().top;
-        const targetLeft = target.getBoundingClientRect().left;
+        const scrollTop = window.pageYOffset || document.documentElement || document.body.scrollTop;
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
+        const targetTop = target.getBoundingClientRect().top + scrollTop;
+        const targetLeft = target.getBoundingClientRect().left + scrollLeft;
         const targetRelTop = targetTop - containerTop;
         const targetRelLeft = targetLeft - containerLeft;
         const targetWidth = target.getBoundingClientRect().width;
@@ -42,12 +44,13 @@ class SimpleLabel {
         const x = targetRelLeft / containerWidth;
         const y = targetRelTop / containerHeight;
 
+
         const labelContainerHeight = this.labelContainer.getBoundingClientRect().height;
         const labelContainerWidth = this.labelContainer.getBoundingClientRect().width;
 
 
-        const labelTop = y < 0.5 ? targetTop + targetHeight : targetTop - targetHeight - labelContainerHeight;
-        const labelLeft = x < 0.5 ? targetLeft + targetWidth : targetLeft - targetWidth - labelContainerWidth;
+        const labelTop = y < 0.5 ? targetTop + targetHeight : targetTop - labelContainerHeight;
+        const labelLeft = x < 0.5 ? targetLeft + targetWidth : targetLeft - labelContainerWidth;
 
         this.labelContainer.style.top = labelTop + "px";
         this.labelContainer.style.left = labelLeft + "px";
@@ -62,13 +65,13 @@ class SimpleLabel {
         this.labelContainer.style.top = "100px";
         this.labelContainer.style.left = "100px";
         this.labelContainer.style.background = "rgba(0,0,0, 0.7)";
-        this.labelContainer.style.padding = "1.3em .7em";
         this.labelContainer.style.borderRadius = "5px";
         this.labelContainer.style.maxHeight = "30%";
         this.labelContainer.style.maxWidth = "30%";
 
         this.textElement.textContent = this.config.text;
         this.textElement.style.color = "white";
+        this.textElement.style.margin = "1.3em .7em";
 
         this.labelContainer.appendChild(this.textElement);
         document.body.appendChild(this.labelContainer);
